4d4e918418e7688eed43d5155e1236cc58b7506c
samupropio1-ship-it <samupropio1@gmail.com>
2026-05-26 14:13:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // BioSpecInfo Service Worker — cache file singolo HTML + risorse + API PubChem/RCSB
 'use strict';
 
-const CACHE_VERSION = 'biospecinfo-v13-2026-05h';
+const CACHE_VERSION = 'biospecinfo-v13-2026-05i';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const API_CACHE = CACHE_VERSION + '-api';
 
